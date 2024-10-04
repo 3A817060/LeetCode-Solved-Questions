29c8b132bcffb4f797d65b427cf51cbcb54b1c02
Guan-Hao Fu <88991064+3A817060@users.noreply.github.com>
2024-10-04 17:50:25 +0800

    Time: 0 ms (100.00%), Space: 15.2 MB (66.75%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
-        vector<vector<string>> alls(1, { "" });
-        
-        for(int j=1; j<=n; j++){
-            vector<string> temp;
-            for(int i=0;i<j;i++)
-                for(string& left : alls[i])
-                    for(string& right : alls[j-1-i])
-                        temp.push_back("(" + left + ")" + right);
-            alls.push_back(temp);
-        }
-        return alls[n];
+        vector<string> result;
+        backtrack(result, "", 0, 0, n);
+        return result;
     }
+    
+    void backtrack(vector<string> &output, string current_str, int open, int close, int n){
+        if(current_str.length() == n*2){
+            output.push_back(current_str);
+            return ;
+        }
+        if(open < n){
+            backtrack(output, current_str + '(', open + 1, close, n);
+        }
+        if(close < open){
+            backtrack(output, current_str + ')', open, close + 1, n);
+        }
+    }
+        
+//         vector<vector<string>> alls(1, { "" });
+        
+//         for(int j=1; j<=n; j++){
+//             vector<string> temp;
+//             for(int i=0;i<j;i++)
+//                 for(string& left : alls[i])
+//                     for(string& right : alls[j-1-i])
+//                         temp.push_back("(" + left + ")" + right);
+//             alls.push_back(temp);
+//         }
+//         return alls[n];
 };
