31e61658fc64e18b923f534802f968fdfb13c654
Guan-Hao Fu <88991064+3A817060@users.noreply.github.com>
2024-11-06 19:50:47 +0800

    Time: 0 ms (100.00%), Space: 58.2 MB (17.34%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -4,16 +4,16 @@ public:
         int left=0;
         int n=nums.size();
         int cnt=0,ans=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0) 
+        for(int r=0;r<n;r++){
+            if(nums[r]==0) 
                 cnt++;
             if(cnt>k){
-                while(nums[left]!=0 && left<i)
+                while(nums[left]!=0 && left<r)
                     left++;
                 left++;
                 cnt--;
             }
-            ans=max(ans,i-left+1);
+            ans=max(ans,r-left+1);
         }
         return ans;
     }
